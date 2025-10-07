ca6e5c7065cda406c83269dd27a764dd229fc6b9
Pablo Strasser <pablo.strasser@hesge.ch>
2025-10-07 23:03:12 +0200

    Reduce batch size

--- build/cfactor.py
+++ build/cfactor.py
@@ -352,7 +352,7 @@ def create_app():
                                                                                     dic['x']=i
                                                                                     dic['y']=j
                                                                                     listvalue.append(dic)
-                                                                                    if len(listvalue)>=10000:
+                                                                                    if len(listvalue)>=1024:
                                                                                           yield listvalue
                                                                                           listvalue=[]
                                                                         if len(listvalue)>0:
